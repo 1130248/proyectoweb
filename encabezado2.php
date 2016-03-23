790d6def96942ee26a9372c8e325c7e652095142
1130248 <1130248@unav.edu.mx>
2016-03-23 10:45:27 -0700

    Update encabezado2.php

--- encabezado2.php
+++ encabezado2.php
@@ -8,7 +8,7 @@
 
 	<div id="encabezado1"> <h1>Ruta 23</h1>					
 	<div id="encabezado2"><h2>Yautepec, Morelos.</h2></div>
-	<div id="bien"> · · · Bienvenido · · ·</div>
+	<div id="bien"> · · · Bienvenido a tu servicio · · ·</div>
 	</div>
 
 </html>
